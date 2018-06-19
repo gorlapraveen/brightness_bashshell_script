5627e17aa2a0d4e15601fa8e5ec04d42882c9535
#PraveenReddyGorla <gorlapraveen@gmail.com>
2018-06-19 05:45:59 +0000

    GNU license txt added

--- backblacklight.sh
+++ backblacklight.sh
@@ -1,8 +1,18 @@
+
 #!/bin/bash
 # backlight brightness controls. use freely
 # and adjust sysfs directory if not on intel
-# $author  praveen reddy gorla
 
+
+#copyright (c) 2016 Author(s): Gorla Praven <gorlapraveen@gmail.com>
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or (at your option) any later version.This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
 sysfs="/sys/class/backlight/intel_backlight"
 max=`cat ${sysfs}/max_brightness`
 level=`cat ${sysfs}/brightness`
