e07f32dc9c85d660f904dd9bf300d53cd0e77ad8
gorlapraveen <gorlapraveen@gmail.com>
2018-07-26 06:42:53 +0700

    Settuping setup.py for global installation

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# backlight brightness controls. use freely
+# and adjust sysfs directory if not on intel
+# $author  praveen reddy gorla
+#setup.py install the backlight brightness packages in your file system so that they are globally available to the user.
+
+$chmod +x backblacklight.sh
+$cp backblacklight /usr/local/bin
